62743b3db9f4d5ec73cd40f2cb3d8d87fdd36eea
Eric Bonnet <eric.d.bonnet@gmail.com>
2015-04-22 18:13:11 +0200

    Update test code.

--- JNaviCell/src/fr/curie/jnavicell/NaviCell.java
+++ JNaviCell/src/fr/curie/jnavicell/NaviCell.java
@@ -1,7 +1,42 @@
 package fr.curie.jnavicell;
 
+import java.awt.List;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.Set;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.NameValuePair;
+
+/**
+ * Java binding for NaviCell REST API.
+ * 
+ * @author eb
+ *
+ */
 public class NaviCell {
 
 	private String map_url = "https://navicell.curie.fr/navicell/maps/cellcycle/master/index.php";
@@ -13,7 +48,11 @@ public class NaviCell {
 	NaviCell() {
 		// nothing to be done here.
 	}
-	
+
+	/**
+	 * Set the map_url address (NaviCell or ACSN map).
+	 * @param url string: valid NaviCell map web address.
+	 */
 	public void setMapUrl(String url) {
 		map_url = url;
 	}
@@ -25,10 +64,96 @@ public class NaviCell {
 	public Set<String> getHugoList() {
 		return(hugo_list);
 	}
+
+	public String getMapUrl() {
+		return(map_url);
+	}
 	
+	public String getProxyUrl() {
+		return(proxy_url);
+	}
 	public String getSessionId () {
 		return(session_id);
 	}
 	
+	@SuppressWarnings("deprecation")
+	public HttpClient buildHttpClient() {
+		HttpClientBuilder b = HttpClientBuilder.create();
+
+		SSLContext sslContext = null;
+		try {
+			sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+				public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+					return true;
+				}
+			}).build();
+			b.setSslcontext( sslContext);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// or SSLConnectionSocketFactory.getDefaultHostnameVerifier(), if you don't want to weaken
+		HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+
+		SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+		        .register("http", PlainConnectionSocketFactory.getSocketFactory())
+		        .register("https", sslSocketFactory)
+		        .build();
+
+		// allows multi-threaded use
+		PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager( socketFactoryRegistry);
+		b.setConnectionManager( connMgr);
+
+		HttpClient client = b.build();
+		return client;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		NaviCell n = new NaviCell();
+		
+		//HttpClient httpclient = HttpClients.createDefault();
+		HttpClient httpclient = n.buildHttpClient();
+		HttpPost httppost = new HttpPost(n.getProxyUrl());
+		
+		// Request parameters and other properties.
+		ArrayList<NameValuePair> params = new ArrayList<NameValuePair>(4);
+		params.add(new BasicNameValuePair("id", "1"));
+		params.add(new BasicNameValuePair("perform", "genid"));
+		params.add(new BasicNameValuePair("msg_id", "1001"));
+		params.add(new BasicNameValuePair("mode", "session"));
+		try {
+			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//Execute and get the response.
+		HttpResponse response = null;
+		try {
+			response = httpclient.execute(httppost);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HttpEntity entity = response.getEntity();
+		try {
+			System.out.println(EntityUtils.toString(entity));
+		} catch (UnsupportedOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 }
